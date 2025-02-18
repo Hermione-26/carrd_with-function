e4b6901ae871f85d85ba504f965f64397111d29e
Hermione-26 <hcjbpalima@gmail.com>
2025-02-18 13:35:04 +0800

    Delete carrd_with-function directory

--- carrd_with-function/java.js
+++
@@ -1,9 +0,0 @@
-// Select the hamburger icon and sidebar elements
-const hamburger = document.querySelector('.hamburger');
-const sidebar = document.querySelector('.sidebar');
-
-// Toggle the 'active' class on both the hamburger and sidebar when clicked
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    sidebar.classList.toggle('active');
-});
